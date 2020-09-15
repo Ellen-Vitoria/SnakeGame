9987b1ffcd872dee9a79ce2db1cc1af4f7e40f7d
ellen <ellen.vitoria.ogs@gmail.com>
2020-09-15 15:58:34 -0300

    Criando função Cobrinha

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
 let canvas = document.getElementById("snake");
 let context = canvas.getContext("2d"); //Plano do Jog
 let box = 32;
+let snake = [];
+snake[0] = {
+    x: 8 * box,
+    y: 8 * box
+}
 
 function criarBG() { //Background
-    context.fillStyle = "lightgreen";
+    context.fillStyle = "purple";
     context.fillRect(0, 0, 16 * box, 16 * box); //Desenho do retângulo, onde acontecerá o jogo - fillRect(X, Y, Altura, Largura)
 }
 
+function criarCobrinha() {
+    for(i=0; i < snake.length; i++){
+        context.fillStyle = "pink";
+        context.fillRect(snake[i].x, snake[i].y, box, box);
+    }
+}
+
 criarBG();
+criarCobrinha();
