89fe771f37386dda55a24a4d54427196ea2f3f2a
ellen <ellen.vitoria.ogs@gmail.com>
2020-09-15 16:15:19 -0300

    Criando função iniciarJogo e estabelecendo movimento à Cobrinha

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ snake[0] = {
     x: 8 * box,
     y: 8 * box
 }
+let direction = "right";
 
 function criarBG() { //Background
     context.fillStyle = "purple";
@@ -19,5 +20,31 @@ function criarCobrinha() {
     }
 }
 
-criarBG();
-criarCobrinha();
+function iniciarJogo() {
+    criarBG();
+    criarCobrinha();
+
+    //Posição da cobrinha
+    let snakeX = snake[0].x;
+    let snakeY = snake[0].y;
+
+    //Coordenadas
+    if(direction == "right") snakeX += box;
+    if(direction == "left") snakeX -= box;
+    if(direction == "up") snakeY -= box;
+    if(direction == "down") snakeY += box;
+
+    //Tirando o último elemento do array
+    snake.pop();
+
+    //Acresentando um no primeiro elemento
+    let newHead = {
+        x: snakeX,
+        y: snakeY
+    }
+    snake.unshift(newHead);
+
+}
+
+let jogo = setInterval(iniciarJogo, 100); //A cada 100 milisegundos vai se renovado e dará continuidade ao Jogo sem travar
+
