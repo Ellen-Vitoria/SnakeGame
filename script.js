cac3bb4991ba22697bbd02de8cc5df6da1a30f48
ellen <ellen.vitoria.ogs@gmail.com>
2020-09-15 16:32:17 -0300

    Movimentação da Cobrinha com as setinhas do teclado

--- script.js
+++ script.js
@@ -20,6 +20,18 @@ function criarCobrinha() {
     }
 }
 
+document.addEventListener('keydown', update); //Evento de uma tecla apertada
+
+/*Condicionais para as setas dos teclados corresponderem a uma direção (por exemplo, 37 significa para esquerda) e
+  impedir que a direção seja oposta, pois a Cobrinha só terá uma cabeça
+*/
+function update (event) {
+    if(event.keyCode == 37 && direction != "right") direction = "left";
+    if(event.keyCode == 38 && direction != "down") direction = "up";
+    if(event.keyCode == 39 && direction != "left") direction = "right";
+    if(event.keyCode == 40 && direction != "up") direction = "down";
+}
+
 function iniciarJogo() {
     criarBG();
     criarCobrinha();
